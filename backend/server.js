09bc193e3802c0b5693e9654b5a184508914d981
Alam Khalid <alamkhalid0803@gmail.com>
2024-04-25 06:04:32 -0700

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import userRoutes from "./routes/user.routes.js";
+import connectDB from "./config/db.js";
+import { errorHandler, notFound } from "./middleware/error.middleware.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+const port = process.env.PORT || 5000;
+
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => res.send("Server is ready"));
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
